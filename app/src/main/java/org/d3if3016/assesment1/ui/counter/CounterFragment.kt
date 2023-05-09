30efe76987903ec16c8b2b8db8d4c75e24ded3f9
AriqNaufalF <ariqn55@gmail.com>
2023-05-09 19:59:04 +0700

    Add stopwatch and reset button

--- app/src/main/java/org/d3if3016/assesment1/ui/counter/CounterFragment.kt
+++ app/src/main/java/org/d3if3016/assesment1/ui/counter/CounterFragment.kt
@@ -2,6 +2,7 @@ package org.d3if3016.assesment1.ui.counter
 
 import android.content.Context
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.Menu
@@ -9,6 +10,8 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -22,6 +25,8 @@ class CounterFragment : Fragment() {
 
     private lateinit var binding: FragmentCounterBinding
     private lateinit var counterAdapter: CounterAdapter
+    private lateinit var chronometer: Chronometer
+    private lateinit var imageButton: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,11 +34,13 @@ class CounterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCounterBinding.inflate(layoutInflater, container, false)
-        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        chronometer = binding.chronometer
+        imageButton = binding.imageButton
         counterAdapter = CounterAdapter(viewModel::updateVehicle)
         //        Set recycler view
         binding.recyclerView.layoutManager =
@@ -44,6 +51,22 @@ class CounterFragment : Fragment() {
         }
         viewModel.getVehiclesData().observe(viewLifecycleOwner) {
             counterAdapter.submitList(it)
+        }
+
+        viewModel.getIsRunning().observe(viewLifecycleOwner) {
+            if (viewModel.getElapsedTime() != 0L && it) {
+                // Jika stopwatch masih berjalan dan konfigurasi diganti lanjutkan waktunya
+                chronometer.base = viewModel.getElapsedTime()
+                imageButton.setImageResource(R.drawable.pause)
+                chronometer.start()
+            } else if (viewModel.getElapsedTime() != 0L) {
+                // Jika stopwatch tidak berjalan serta waktu tidak sama dengan 0 dan konfigurasi diganti kembalikan waktu semula
+                chronometer.base = viewModel.getStartTime() + SystemClock.elapsedRealtime()
+            }
+        }
+
+        imageButton.setOnClickListener {
+            startStopTimer()
         }
     }
 
@@ -58,6 +81,13 @@ class CounterFragment : Fragment() {
                 // TODO: to my data fragment
                 return true
             }
+            R.id.refresh_menu -> {
+                viewModel.resetData()
+                chronometer.base = SystemClock.elapsedRealtime()
+                chronometer.stop()
+                imageButton.setImageResource(R.drawable.play_arrow)
+                return true
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -67,5 +97,23 @@ class CounterFragment : Fragment() {
         val displayMetrics: DisplayMetrics = context.resources.displayMetrics
         val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
         return (screenWidthDp / columnWidthDp + 0.5).toInt()
+    }
+
+    private fun startStopTimer() {
+        if (viewModel.getIsRunning().value!!) {
+            // Update variable start time saat tombol pause ditekan
+            viewModel.setStartTime(chronometer.base - SystemClock.elapsedRealtime())
+            chronometer.stop()
+            imageButton.setImageResource(R.drawable.play_arrow)
+            viewModel.setIsRunning(false)
+        } else {
+            // Mendapatkan waktu yang sudah berlalu mulai dari start time
+            val elapsedTime = SystemClock.elapsedRealtime() + viewModel.getStartTime()
+            viewModel.setElapsedTime(elapsedTime)
+            chronometer.base = elapsedTime
+            imageButton.setImageResource(R.drawable.pause)
+            chronometer.start()
+            viewModel.setIsRunning(true)
+        }
     }
 }
--- app/src/main/java/org/d3if3016/assesment1/ui/counter/CounterViewModel.kt
+++ app/src/main/java/org/d3if3016/assesment1/ui/counter/CounterViewModel.kt
@@ -7,6 +7,16 @@ import org.d3if3016.assesment1.data.getVehicles
 
 class CounterViewModel : ViewModel() {
     private val vehicles = MutableLiveData(getVehicles())
+    private var elapsedTime = 0L
+    private var startTime = 0L
+    private var isRunning = MutableLiveData(false)
+
+    fun resetData() {
+        vehicles.value = getVehicles()
+        elapsedTime = 0L
+        startTime = 0L
+        isRunning.value = false
+    }
 
     fun updateVehicle(vehicle: Vehicle) {
         vehicles.value = vehicles.value?.let {
@@ -17,5 +27,20 @@ class CounterViewModel : ViewModel() {
         }
     }
 
+    fun setElapsedTime(elapsedTime: Long) {
+        this.elapsedTime = elapsedTime
+    }
+
+    fun setIsRunning(isRun: Boolean) {
+        isRunning.value = isRun
+    }
+
+    fun setStartTime(startTime: Long) {
+        this.startTime = startTime
+    }
+
     fun getVehiclesData() = vehicles
+    fun getElapsedTime() = elapsedTime
+    fun getStartTime() = startTime
+    fun getIsRunning() = isRunning
 }
